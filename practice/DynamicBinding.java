5dd676c90aa0b29c27be0c6ee39de758d0a66a3a
kuntal1461 <kuntal.1461@gmail.com>
2025-09-20 17:15:32 +0530

    Dynamic Biding

--- practice/DynamicBinding.java
+++ practice/DynamicBinding.java
@@ -1,3 +1,22 @@
 public class DynamicBinding {
+
+    public static void main(String[] args) {
+      // B b= new B();
+      // b.show();
+
+       A a= new B();
+       a.show();
+    }
     
 }
+class A {
+        public  void show() {
+            System.out.println("A show");
+        }    
+    }
+class  B extends A {
+        public void show(){
+            System.out.println("B Show");
+        }
+        
+ }
