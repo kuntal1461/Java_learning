d5ec1953f73d0aebb7c467b98792a4819d9e2f51
kuntal1461 <kuntal.1461@gmail.com>
2025-09-21 13:14:23 +0530

    By Kuntal : Commit

--- interviewBite/Two_Integer_Sum.java
+++ interviewBite/Two_Integer_Sum.java
@@ -1,13 +1,41 @@
 package interviewBite;
 
+import java.util.Arrays;
+
 public class Two_Integer_Sum {
 
     public static void main(String[] args) {
         
-        int [] nums= {1,2,3,4,5,6,7};
+        int [] nums= {1,2,3,4,5,6,9};
         int target=8;
 
+        int[] result = checkTheNum(nums,target);
 
+        System.out.println( "the result is "+Arrays.toString(result));
+
+
+    }
+    private static int[] checkTheNum(int[] nums, int target) {
+
+        int i=0;
+        int j= nums.length-1;
+
+        while (i<j) {
+
+           int currentSum = nums[i]+nums[j];
+
+           if (currentSum>target) {
+            j--;
+           }
+           else if (currentSum<target) {
+            i++;
+           }
+           else{
+            return new int[] {nums[i],nums[j]};
+           }
+           
+        }
+        return new int[0];
 
     }
     
