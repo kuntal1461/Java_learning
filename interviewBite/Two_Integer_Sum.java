5640a5564ba116313986ac47770970c9bb5117e8
kuntal1461 <kuntal.1461@gmail.com>
2025-09-21 13:01:52 +0530

    By Kuntal :Chnages

---
+++ interviewBite/Two_Integer_Sum.java
@@ -0,0 +1,14 @@
+package interviewBite;
+
+public class Two_Integer_Sum {
+
+    public static void main(String[] args) {
+        
+        int [] nums= {1,2,3,4,5,6,7};
+        int target=8;
+
+
+
+    }
+    
+}
--- practice/main.java
+++ practice/main.java
@@ -2,15 +2,24 @@ public class main {
     
     public static void main(String[] args) {
         
+        String s = "Was it a car or a cat I saw?";
 
-        ThredClassWithExteds t1 = new ThredClassWithExteds();
+        checkPlindrom(s);
 
-        t1.start();
+    }
 
-        ThredClassWithRunable t2= new ThredClassWithRunable();
+    private static boolean checkPlindrom(String s) {
 
-        Thread thread = new Thread(t2);
-        thread.start();
+        StringBuilder newString = new StringBuilder();
+        for(int i=0;i<s.length();i++){
+           char charofS = s.charAt(i);
+            if (Character.isLetterOrDigit(charofS)) {
+                newString.append(Character.toLowerCase(charofS));
+                
+            }
+        }
+        String reverseString = newString.reverse().toString();
+        return newString.toString().equals(reverseString);
 
     }
 }
