c11256418e3df67c004d53213dbf14da5a37042f
kuntal1461 <kuntal.1461@gmail.com>
2025-09-21 20:29:32 +0530

    By Kuntal : code changes

---
+++ practice/validParentheses.java
@@ -0,0 +1,42 @@
+import java.util.Stack;
+
+public class validParentheses {
+    
+
+
+    public static void main(String[] args) {
+        
+        String s = "()[]{}";
+
+        isValid(s);
+    }
+
+    private static boolean isValid(String s) {
+
+        Stack stack= new Stack<Character>();
+
+        for(int i =0;i < s.length();i++){
+           char newChar = s.charAt(i);
+
+           if (newChar==('(') || newChar==('{') || newChar==('[')) {
+            stack.push(newChar);
+            
+           }
+
+           Character topChar = (Character) stack.pop();
+
+           if (newChar =='(' & topChar != ')' ) {
+                return false;
+           }
+           if (newChar =='{' & topChar != '}' ) {
+                return false;
+           }
+           if (newChar =='[' & topChar != ']' ) {
+                return false;
+           }
+
+        }
+        return stack.isEmpty();
+    }
+    
+}
